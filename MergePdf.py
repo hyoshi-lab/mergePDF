a6df1a361205540fa6594458a30003d600906360
hyoshi <yoshi@nagaokauniv.ac.jp>
2021-12-10 09:57:42 +0900

    位置補正ボタン　追加

--- MergePdf.py
+++ MergePdf.py
@@ -4,8 +4,8 @@ Created on Sat Jun 19 14:06:21 2021
 
 @author: yoshi@nagaokauniv.ac.jp
 '''
-version = 'ver.0.94'
-#ver.0.94   20211209    コニカミノルタ複合機　ページ位置対応
+version = 'ver.0.95'
+#ver.0.94   20211210    ページ位置オプション_1 座標，倍率修正
 
 import sys
 import os
@@ -31,7 +31,7 @@ pageNumber = False
 headerX = 0
 headerY = 10
 fontSize = 8
-option_1 = 0     # コニカミノルタ複合機
+option_1 = 0     # ページ番号位置補正
 
 def readConfigFile():
     global inputPath
@@ -168,9 +168,9 @@ def conductMain():
             if pageNumber :
                 text += ' [{:3}/{:3}]'.format(num + 1, pageCount)
             tw = fitz.TextWriter(page.rect)
-            # 画像ページ位置補正用
+            # 座標，倍率修正
             imgList = page.getImageList()
-            if option_1 and (0 < len(imgList)):
+            if option_1 and (0 < len(imgList)): # ページ番号位置補正
                 for j, img in enumerate(page.getImageList()):
                     imgInfo = pdf_writer.extractImage(img[0])
                 hImage = imgInfo['height']
@@ -258,7 +258,7 @@ if __name__ == '__main__':
     frame5.pack()
     bOption_1 = BooleanVar()
     bOption_1.set(option_1)
-    chk = ttk.Checkbutton(frame5, variable=bOption_1, text='位置補正_1')
+    chk = ttk.Checkbutton(frame5, variable=bOption_1, text='位置補正')
     chk.pack(side=LEFT)
 
     #リストボックスの作成
